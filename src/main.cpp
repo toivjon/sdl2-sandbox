9150d248a3695e837ec432c1a8d51dbd5b50ab05
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-01-02 21:33:30 +0200

    Added some initial notes about SDL2 initialization and shutdown.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,32 @@
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    // ========================================================================
+    // Initialize the SDL along with desired subsystems.
+    // SDL_INIT_TIMER............Include SDL timer support.
+    // SDL_INIT_AUDIO............Include SDL audio (???) support.
+    // SDL_INIT_VIDEO............Include SDL video/graphics support.
+    // SDL_INIT_JOYSTICK.........Include SDL joystick support.
+    // SDL_INIT_HAPTIC...........Include SDL haptic (force feedback) support.
+    // SDL_INIT_GAMECONTROLLER...Include SDL game controller support.
+    // SDL_INIT_EVENTS...........Include SDL event system.
+    // SDL_INIT_EVERYTHING.......Include all SDL subsystems.
+    // 
+    // Some subsystems also automatically imply other subsystems.
+    // SDL_INIT_GAMECONTROLLER...Implies SDL_INIT_JOYSTICK.
+    // SDL_INIT_VIDEO............Implies SDL_INIT_EVENTS.
+    // SDL_INIT_JOYSTICK.........Implies SDL_INIT_EVENTS.
+    // ========================================================================
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Failed to initialize SDL: %s\n", SDL_GetError());
+        return -1;
+    }
 
     // ...
 
-    SDL_Quit();   
+    // ========================================================================
+    // Shut down all SDL subsystems.
+    SDL_Quit();
+    
     return 0;
 }
