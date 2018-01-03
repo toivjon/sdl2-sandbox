aa8de100cac94da227cc4f170f916c64d634065e
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-01-03 22:25:24 +0200

    Added some notes about how to check whether some SDL subsystems are initialized.

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,28 @@ int main(int argc, char* argv[])
     // SDL_INIT_GAMECONTROLLER...Implies SDL_INIT_JOYSTICK.
     // SDL_INIT_VIDEO............Implies SDL_INIT_EVENTS.
     // SDL_INIT_JOYSTICK.........Implies SDL_INIT_EVENTS.
+    //
+    // Definitions can be OR'd together (i.e. SDL_INIT_TIMER | SDL_INIT_AUDIO)
     // ========================================================================
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         SDL_Log("Failed to initialize SDL: %s\n", SDL_GetError());
         return -1;
     }
+
+    // ========================================================================
+    // SDL offers a way to check which SDL subsystems has been initialized.
+    // Uses the same macros than what are used with SDL_Init (see above).
+    // 
+    // Definitions can be OR'd together (i.e. SDL_INIT_TIMER | SDL_INIT_AUDIO)
+    // ========================================================================
+    SDL_Log("Initialized SDL subsystems:\n");
+    SDL_Log("[%d] Timer\n", SDL_WasInit(SDL_INIT_TIMER) != 0);
+    SDL_Log("[%d] Audio\n", SDL_WasInit(SDL_INIT_AUDIO) != 0);
+    SDL_Log("[%d] Video\n", SDL_WasInit(SDL_INIT_VIDEO) != 0);
+    SDL_Log("[%d] Joystick\n", SDL_WasInit(SDL_INIT_JOYSTICK) != 0);
+    SDL_Log("[%d] Haptic\n", SDL_WasInit(SDL_INIT_HAPTIC) != 0);
+    SDL_Log("[%d] Game controller\n", SDL_WasInit(SDL_INIT_GAMECONTROLLER) != 0);
+    SDL_Log("[%d] Events\n", SDL_WasInit(SDL_INIT_EVENTS) != 0);
 
     // ...
 
